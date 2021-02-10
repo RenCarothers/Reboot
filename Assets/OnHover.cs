c88075fa2fd125a7cc41935ca9420662b4c9b398
Ren Carothers <rencarothers@gmail.com>
2021-02-09 22:32:56 -0800

    still fiddling with mouse and hover settings

---
+++ Assets/OnHover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHover : MonoBehaviour
+{   
+    public SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public virtual void OnMouseOver() 
+    { 
+       sprite.color = new Color (1, 0, 1, 1);
+    } 
+    
+    public virtual void OnMouseExit() 
+    {
+        sprite.color = new Color (1, 1, 1, 1);
+    } 
+}
+
+// https://forum.unity.com/threads/spriterenderer-color-doesnt-work-bug.721547/
